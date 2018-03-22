c6db195ac93f7ad0e75788c17218ef2dce8286d1
nextonedaygg <nextonedaygg@163.com>
2018-03-22 18:19:51 +0800

    修改成一个fragment页面

--- app/src/main/java/com/nextoneday/customview/MainActivity.java
+++ app/src/main/java/com/nextoneday/customview/MainActivity.java
@@ -2,6 +2,10 @@ package com.nextoneday.customview;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.nextoneday.customview.bean.PieData;
@@ -13,18 +17,45 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private PieView mPieview;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mPieview = findViewById(R.id.pieview);
 
+        initView();
         initData();
-
 
     }
 
+    private void initView() {
+        mViewPager = findViewById(R.id.viewpager);
+        TabLayout tablayout =findViewById(R.id.tablayout);
+
+    }
+
+    private void  initDatas(){
+
+        ArrayList<Fragment> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+
+        }
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+        });
+
+
+
+    }
     private void initData() {
 
         ArrayList<PieData> arrayList= new ArrayList<>();
