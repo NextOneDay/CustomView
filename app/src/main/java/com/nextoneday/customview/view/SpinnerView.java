9bca5616d6d59dc3b9f59c0f5f73cbf3309d701b
nextonedaygg <nextonedaygg@163.com>
2018-03-23 15:29:17 +0800

    添加一个下拉选择的控件

--- app/src/main/java/com/nextoneday/customview/view/SpinnerView.java
+++ app/src/main/java/com/nextoneday/customview/view/SpinnerView.java
@@ -1,23 +1,34 @@
 package com.nextoneday.customview.view;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.nextoneday.customview.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2018/3/23.
  */
 
-public class SpinnerView extends RelativeLayout implements View.OnClickListener {
+public class SpinnerView extends RelativeLayout implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     private ImageView mIvArr;
     private EditText mEditText;
+    private ArrayList<String> mdata;
+    private PopupWindow mWindow;
 
     public SpinnerView(Context context) {
         super(context);
@@ -39,8 +50,26 @@ public class SpinnerView extends RelativeLayout implements View.OnClickListener 
         mEditText = view.findViewById(R.id.ed_text);
         mIvArr = view.findViewById(R.id.iv_arr);
 
+        initData();
+
         initEvent();
     }
+
+    private void initData() {
+        mdata = new ArrayList<>();
+        for (int x = 10000; x < 10040; x++) {
+            mdata.add(x + "");
+        }
+    }
+    public void setData(ArrayList<String> al){
+        this.mdata=mdata;
+    }
+
+    /**
+     * 还应该提供一个选择回调监听
+     *
+     */
+
 
     private void initEvent() {
         mIvArr.setOnClickListener(this);
@@ -57,5 +86,83 @@ public class SpinnerView extends RelativeLayout implements View.OnClickListener 
      */
     private void showWindow() {
 
+        if (mWindow == null) {
+            mWindow = new PopupWindow();
+            mWindow.setWidth(mEditText.getWidth());
+            mWindow.setHeight(LayoutParams.WRAP_CONTENT);
+            ListView contentView = new ListView(getContext());
+            contentView.setVerticalScrollBarEnabled(true);
+            contentView.setAdapter(mAdapter);
+            mWindow.setContentView(contentView);
+            contentView.setOnItemClickListener(this);
+
+            mWindow.setFocusable(true);
+            mWindow.setBackgroundDrawable(new ColorDrawable());
+            mWindow.setOutsideTouchable(true);
+        }
+
+        mWindow.showAsDropDown(mEditText);
+
+    }
+
+    private BaseAdapter mAdapter = new BaseAdapter() {
+
+        @Override
+        public int getCount() {
+            return mdata == null ? 0 : mdata.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, android.view.View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_list, null);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            final String text = mdata.get(position);
+            holder.tvText.setText(text);
+            holder.ivDelete.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mdata.remove(text);
+                    notifyDataSetChanged();
+                }
+            });
+
+            return convertView;
+        }
+    };
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String s = mdata.get(position);
+        mEditText.setText(s);
+        mEditText.setSelection(s.length());
+        mWindow.dismiss();
+    }
+
+    private class ViewHolder {
+        ImageView ivLogo;
+        TextView tvText;
+        ImageView ivDelete;
+
+        ViewHolder(View rootview) {
+            ivLogo = rootview.findViewById(R.id.iv_logo);
+            tvText = rootview.findViewById(R.id.tv_text);
+            ivDelete = rootview.findViewById(R.id.iv_delete);
+        }
     }
 }
