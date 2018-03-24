b353b0c75e02ab2712dfc37be9ee32a651eed150
nextonedaygg <nextonedaygg@163.com>
2018-03-24 22:34:17 +0800

    优化文字位置

--- app/src/main/java/com/nextoneday/customview/view/CircleLoadingView.java
+++ app/src/main/java/com/nextoneday/customview/view/CircleLoadingView.java
@@ -10,6 +10,7 @@ import android.nfc.Tag;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.transition.ArcMotion;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -52,9 +53,9 @@ public class CircleLoadingView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mWidth= w;
-        mHeight =h;
-        Log.d(TAG,"mwidth: "+mWidth+": "+"mheight:"+mHeight);
+        mWidth = w;
+        mHeight = h;
+        Log.d(TAG, "mwidth: " + mWidth + ": " + "mheight:" + mHeight);
         initData();
     }
 
@@ -63,10 +64,9 @@ public class CircleLoadingView extends View {
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setColor(Color.CYAN);
-        mPaint.setStrokeWidth(5);
+        mPaint.setStrokeWidth(8);
 
-        mTextPaint =new Paint();
+        mTextPaint = new Paint();
 
         mTextPaint.setAntiAlias(true);
         mTextPaint.setStyle(Paint.Style.FILL);
@@ -76,15 +76,15 @@ public class CircleLoadingView extends View {
     }
 
     int size = 5;
+
     private void initData() {
 
-        Log.d(TAG,"l , t, r, b :"+ getLeft()+" "+ getTop()+" "+getRight()+" "+getBottom());
+        Log.d(TAG, "l , t, r, b :" + getLeft() + " " + getTop() + " " + getRight() + " " + getBottom());
 //        mRect = new RectF(getLeft()+10,getTop()+10,getRight()-10,getBottom()-10);
 
-        mRect = new RectF(0+size,0+size,mWidth-size,mHeight-size);
+        mRect = new RectF(0 + size, 0 + size, mWidth - size, mHeight - size);
 
     }
-
 
 
     @Override
@@ -92,14 +92,25 @@ public class CircleLoadingView extends View {
         super.onDraw(canvas);
 
         //先画个百分百的灰色的圆
+        mPaint.setColor(Color.GRAY);
+        canvas.drawArc(mRect, -90, 360, false, mPaint);
+
+        //画进度圆
+        mPaint.setColor(Color.CYAN);
+        int sweepAngle = (int) ((progress / 100f) * 360);
+        canvas.drawArc(mRect, -90, sweepAngle, false, mPaint);
 
 
-        int sweepAngle= (int) ((progress/100f)*360);
-        canvas.drawArc(mRect,-90,sweepAngle,false,mPaint);
-
-
-        canvas.drawText(progress+" %", mWidth/2,
-                mHeight/2+size,mTextPaint);
+        //计算文字位置，并画文字
+        float textwidth = mWidth / 2 ;
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        float textHeight = mHeight / 2 + (Math.abs(fontMetrics.ascent) - fontMetrics.descent) / 2;
+        if(progress==0){
+            mTextPaint.setColor(Color.GRAY);
+        }else {
+            mTextPaint.setColor(Color.CYAN);
+        }
+        canvas.drawText(progress + "%", textwidth, textHeight, mTextPaint);
     }
 
     @Override
@@ -107,12 +118,12 @@ public class CircleLoadingView extends View {
 
         float x = event.getX();
         float y = event.getY();
-        Log.d(TAG, x+":::"+y);
-        return  super.onTouchEvent(event);
+        Log.d(TAG, x + ":::" + y);
+        return super.onTouchEvent(event);
     }
 
     public void setprogress(int i) {
-        this.progress=i;
+        this.progress = i;
         postInvalidate();
     }
 }
