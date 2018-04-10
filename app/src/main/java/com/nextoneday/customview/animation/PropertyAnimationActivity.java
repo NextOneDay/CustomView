46cd8b099edb914f1cf4b69e6ee29da9e2fddf69
nextonedaygg <nextonedaygg@163.com>
2018-04-10 22:21:37 +0800

    新增view动画

--- app/src/main/java/com/nextoneday/customview/animation/PropertyAnimationActivity.java
+++ app/src/main/java/com/nextoneday/customview/animation/PropertyAnimationActivity.java
@@ -3,6 +3,8 @@ package com.nextoneday.customview.animation;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.nextoneday.customview.R;
 
@@ -10,10 +12,34 @@ import com.nextoneday.customview.R;
  * Created by Administrator on 2018/4/10.
  */
 
-public class PropertyAnimationActivity extends AppCompatActivity {
+public class PropertyAnimationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View mView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property);
+
+        mView = findViewById(R.id.view);
+        Button start = findViewById(R.id.start);
+        start.setOnClickListener(this);
+
+
+    }
+
+    private void animatorSet_code() {
+
+    }
+
+    private void animatorSet_xml() {
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        animatorSet_code();
+        animatorSet_xml();
     }
 }
--- app/src/main/java/com/nextoneday/customview/animation/ViewAnimationActivity.java
+++ app/src/main/java/com/nextoneday/customview/animation/ViewAnimationActivity.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
 import com.nextoneday.customview.R;
@@ -15,7 +21,7 @@ import com.nextoneday.customview.R;
  * Created by Administrator on 2018/4/10.
  */
 
-public class ViewAnimationActivity  extends AppCompatActivity implements View.OnClickListener {
+public class ViewAnimationActivity extends AppCompatActivity implements View.OnClickListener {
 
     private View mView;
     private Button mStart;
@@ -26,7 +32,7 @@ public class ViewAnimationActivity  extends AppCompatActivity implements View.On
         setContentView(R.layout.activity_view);
         mView = findViewById(R.id.view);
         mStart = findViewById(R.id.start);
-       mStart.setOnClickListener(this);
+        mStart.setOnClickListener(this);
     }
 
     private void set_xml() {
@@ -35,21 +41,51 @@ public class ViewAnimationActivity  extends AppCompatActivity implements View.On
 
         mView.startAnimation(animation);
     }
-    private void set_code(){
 
+    private void set_code() {
+
+        AnimationSet set = new AnimationSet(false);
+
+        RotateAnimation rotate = new RotateAnimation(120, 360);
+        rotate.setInterpolator(new LinearInterpolator());
+
+        ScaleAnimation scale = new ScaleAnimation(1,1,1,0.5f);
+        scale.setDuration(1500);
+
+        set.addAnimation(scale);
+        set.addAnimation(rotate);
+        set.setDuration(3000);
+        mView.startAnimation(set);
 
     }
 
-    private void rotate() {
 
+
+    private void scale_code() {
+
+        ScaleAnimation scale = new ScaleAnimation(1.0f,1.5f,1.0f,1.5f);
+        scale.setDuration(2000);
+        scale.setFillAfter(true);
+        mView.startAnimation(scale);
     }
 
-    private void scale() {
+    private void scale_xml(){
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.scale_animation);
+        mView.startAnimation(animation);
+
 
     }
+    private void translate_code() {
 
-    private void translate() {
-
+        TranslateAnimation translate = new TranslateAnimation(0,400,0,400);
+        translate.setFillAfter(true);
+        translate.setDuration(3000);
+        mView.startAnimation(translate);
+    }
+    private void translate_xml(){
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.translate_animation);
+        mView.startAnimation(animation);
     }
 
     private void alpha_xml() {
@@ -59,15 +95,17 @@ public class ViewAnimationActivity  extends AppCompatActivity implements View.On
 
     }
 
-    private void alpha_code(){
+    private void alpha_code() {
 
-        AlphaAnimation animation= new AlphaAnimation(1,0);
+        AlphaAnimation animation = new AlphaAnimation(1, 0);
         animation.setDuration(2000);
+        animation.setRepeatCount(2);
+        animation.setFillBefore(true);
         mView.startAnimation(animation);
     }
 
     @Override
     public void onClick(View v) {
-       set_xml();
+        translate_xml();
     }
 }
