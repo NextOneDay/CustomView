78ce46e9f27f4fe8da9e855bf0d10bc7366eda4e
nextonedaygg <nextonedaygg@163.com>
2018-04-10 22:45:02 +0800

    新增属性动画set

--- app/src/main/java/com/nextoneday/customview/animation/PropertyAnimationActivity.java
+++ app/src/main/java/com/nextoneday/customview/animation/PropertyAnimationActivity.java
@@ -1,5 +1,10 @@
 package com.nextoneday.customview.animation;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -28,11 +33,31 @@ public class PropertyAnimationActivity extends AppCompatActivity implements View
 
     }
 
+    @SuppressLint("ObjectAnimatorBinding")
     private void animatorSet_code() {
 
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator translation = ObjectAnimator.ofFloat(mView, "translation", 0, 100);
+        translation.setDuration(3000);
+        ObjectAnimator rotation = ObjectAnimator.ofFloat(mView, "rotation", 10, 360);
+        rotation.setDuration(3000);
+
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(mView, "alpha", 1.0f, 0.5f);
+
+        alpha.setDuration(3000);
+
+
+        animatorSet.playTogether(translation,rotation,alpha);
+        animatorSet.start();
     }
 
     private void animatorSet_xml() {
+
+        Animator animator = AnimatorInflater.loadAnimator(this,  R.animator.set_animator);
+        animator.setTarget(mView);
+        animator.setDuration(5000);
+        animator.start();
 
 
     }
@@ -40,6 +65,6 @@ public class PropertyAnimationActivity extends AppCompatActivity implements View
     @Override
     public void onClick(View v) {
         animatorSet_code();
-        animatorSet_xml();
+//        animatorSet_xml();
     }
 }
